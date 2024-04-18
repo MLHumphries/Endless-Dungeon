a5757180b5d526952d374325c3c09e7a9f8a024a
Mitchell Humphries <mitchgt@comcast.net>
2024-04-18 13:47:59 -0500

    Adjusted Overworld
    -Added an overworld
    -Added screen to warn player of combat
    -Loads into combat.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
@@ -9,8 +10,10 @@ public class PlayerController : MonoBehaviour {
     public float turnSpeed;
     private Rigidbody rbdy;
 
-    public int combatChance;
+    public GameObject flashScreen;
 
+    public int combatChance;
+    private bool isFroze = false;
 	// Use this for initialization
 	void Start ()
     {
@@ -21,16 +24,31 @@ public class PlayerController : MonoBehaviour {
 	void Update ()
     {
         float moveVertical = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        transform.Translate (new Vector3(0, 0, moveVertical));
+        
 
         float turn = Input.GetAxis("Horizontal") * Time.deltaTime * turnSpeed;
-        transform.Rotate (new Vector3 (0, turn, 0));
+        
+        if (isFroze)
+        {
+            moveVertical = 0;
+            turn = 0;
+        }
+        transform.Translate(new Vector3(0, 0, moveVertical));
+        transform.Rotate(new Vector3(0, turn, 0));
 
-        if(turn < .1)
+        if (turn < .1)
         {
             rbdy.angularVelocity = Vector3.zero;
         }
-
+        if(flashScreen != null)
+        {
+            if(flashScreen.GetComponent<Image>().color.a > 0)
+            {
+                var color = flashScreen.GetComponent<Image>().color;
+                color.a -= 0.01f;
+                flashScreen.GetComponent<Image>().color = color;
+            }
+        }
         
     }
     //Triggers combat
@@ -40,13 +58,33 @@ public class PlayerController : MonoBehaviour {
 
         if (combatChance > 0)
         {
-            SceneManager.LoadScene("Combat");
+            isFroze = true;
+            ChangeAlphaColor();
+            Invoke("LoadCombat", 3.0f);
         }
         else
         {
             Debug.Log("No Combat");
         }
 
+    }
+
+    private void ChangeAlphaColor()
+    {
+        var color = flashScreen.GetComponent<Image>().color;
+        color.a = 0.8f;
+        flashScreen.GetComponent<Image>().color = color;
+        //SceneManager.LoadScene("Combat");
+    }
+
+    private IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(3.5f);
+    }
+
+    private void LoadCombat()
+    {
+        SceneManager.LoadScene("Combat");
     }
 
 
